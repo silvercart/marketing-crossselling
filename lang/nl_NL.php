f89767c81d2edbf30df6140f194ebc5689fc7088
Sascha Koehler <skoehler@pixeltricks.de>
2012-04-11 09:34:40 +0200

    ENHANCEMENT Added dutch translation file (community contribution)

---
+++ lang/nl_NL.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright 2011 pixeltricks GmbH
+ *
+ * This file is part of SilverCart.
+ *
+ * SilverCart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SilverCart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SilverCart.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * English (US) language pack
+ *
+ * @package SilvercartMarketingCrossSelling
+ * @subpackage i18n
+ * @author Sascha Koehler <skoehler@pixeltricks.de>
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @ignore
+ */
+
+
+i18n::include_locale_file('silvercart', 'en_US');
+
+global $lang;
+
+if (array_key_exists('nl_NL', $lang) && is_array($lang['nl_NL'])) {
+    $lang['nl_NL'] = array_merge($lang['en_US'], $lang['nl_NL']);
+} else {
+    $lang['nl_NL'] = $lang['en_US'];
+}
+
+
+
+
+
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['CHOOSE_FILL_METHOD']           = 'Geef aan hoe producten moeten worden geselecteerd voor de widget';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['CMSTITLE']                     = 'Widget voor het weergeven van cross-selling producten';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['CUSTOM_TEMPLATE_NAME']         = 'Naam van de aangepaste sjabloon (zonder .ss extensie)';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['DESCRIPTION']                  = 'Toont de gekozen producten in een widget.';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['FILL_METHOD']                  = 'Selectiemethode voor producten';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['IS_CONTENT_VIEW']              = 'Gebruik gewone Productoverzicht in plaats van widgetview';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['NUMBER_OF_PRODUCTS']           = 'Aantal producten laten zien';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['OTHERPRODUCTGROUP']            = 'Gebruik producten uit de volgende productgroep';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['TITLE']                        = 'Widget voor het weergeven van cross-selling producten';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['ORDERSTATISTICS']              = 'Gebruik om statistieken op producten die zijn gekocht te tonen samen met het bekeken product';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['RANDOMGENERATOR']              = 'Gebruik toevalsgenerator (producten worden willekeurig gekozen uit dezelfde productgroep)';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['SHOW_ON_PRODUCT_GROUP_PAGES']  = 'Toon ook op productgroep pagina\'s';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['USE_LISTVIEW']                 = 'Gebruik lijstweergave';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['WIDGET_TITLE']                 = 'Widget titel';
+$lang['nl_NL']['SilvercartMarketingCrossSellingWidget']['USE_CUSTOM_TEMPLATES']         = 'Gebruik aangepaste sjabloon voor het weergeven van';
